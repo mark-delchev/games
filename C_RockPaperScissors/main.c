856e90aa9a32bba18dbf3553047cc0eafa0a9453
Mark Delchev <markdelchev@gmail.com>
2022-08-25 11:06:03 +0300

    random choice update

--- C_RockPaperScissors/main.c
+++ C_RockPaperScissors/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
 int main()
 {
-    srand(time(NULL));
+    srand(time(0));
     int computer_choice = rand() % (2 + 1 - 0) + 0;
     char choice[][20] = {"rock", "paper", "scissors"};
-    printf("%s", choice[1]);
+    char computer_input[10] = choice[computer_choice];
+    printf(computer_input);
     return 0;
 }
