cb33e95c5dec2f4aa5a1189cc6da0ec5c4d73546
Mark Delchev <markdelchev@gmail.com>
2022-09-11 20:12:44 +0300

    fixed computer randomness

--- C_RockPaperScissors/main.c
+++ C_RockPaperScissors/main.c
@@ -4,11 +4,30 @@
 
 int main()
 {
+    int player_choice;
+    char player_input;
     srand(time(0));
-    int computer_choice = rand() % (2 + 1 - 0) + 0;
-    char choice[][20] = {"rock", "paper", "scissors"};
-    char computer_input[10] = choice[computer_choice];
-    printf(computer_input);
-    printf(computer_choice);
+    int computer_choice = rand() % 300;
+    printf("%d\n", computer_choice);
+    if (computer_choice < 100)
+    {
+        computer_choice = 0;
+    }
+    else if (computer_choice >= 100 && computer_choice < 200)
+    {
+        computer_choice = 1;
+    }
+    else
+    {
+        computer_choice = 2;
+    }
+    scanf("%s", player_input);
+    if (player_input = "paper")
+    {
+        player_choice = 0;
+    }
+    printf("%d", computer_choice);
+    printf("%d", player_choice);
     return 0;
+
 }
