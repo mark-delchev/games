30bc577b769281f452e0e723e0c27d9d998a7090
Mark Delchev <markdelchev@gmail.com>
2022-09-12 16:52:45 +0300

    implemented ties

--- C_RockPaperScissors/main.c
+++ C_RockPaperScissors/main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-    int player_choice;
-    char player_input;
+    char player_input[10];
+    char computer_input[10];
     srand(time(0));
     int computer_choice = rand() % 300;
-    printf("%d\n", computer_choice);
     if (computer_choice < 100)
     {
         computer_choice = 0;
@@ -22,12 +22,24 @@ int main()
         computer_choice = 2;
     }
     scanf("%s", player_input);
-    if (player_input = "paper")
+    int player_choice = 0;
+    if (strcmp(player_input, "rock") == 0)
     {
         player_choice = 0;
     }
-    printf("%d", computer_choice);
-    printf("%d", player_choice);
+    else if (strcmp(player_input, "paper") == 0)
+    {
+        player_choice = 1;
+    }
+    else
+    {
+        player_choice = 2;
+    }
+    if (player_choice == computer_choice)
+    {
+        printf("Tie!\n");
+        printf("Player: %s\n", player_input);
+    }
     return 0;
 
 }
